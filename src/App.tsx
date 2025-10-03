ce366c81430e3c045924ac0c6fa17dc1013b667c
tuanqchau <tuanqchau2810@gmail.com>
2025-10-02 21:13:27 -0400

    add transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Dashboard from './pages/Dashboard';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 function App() {
 
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Dashboard />
-    </>
+    </LocalizationProvider>
   )
 }
 
