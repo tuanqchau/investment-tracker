94a396598b9abb342b21d60b560afde804bf0ee8
tuanqchau <tuanqchau2810@gmail.com>
2025-10-03 22:36:16 -0400

    connect supabase

---
+++ src/supabaseClient.ts
@@ -0,0 +1,6 @@
+import {createClient, SupabaseClient} from '@supabase/supabase-js';
+
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
